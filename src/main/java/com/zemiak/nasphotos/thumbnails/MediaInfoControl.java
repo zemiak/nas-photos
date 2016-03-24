9007a0842e80c624bfc17a8670abea9c602f2bb4
zemiak <zemiak@gmail.com>
2016-03-24 20:56:40 +0100

    Trying to rotate movie thumbnails/2

---
+++ src/main/java/com/zemiak/nasphotos/thumbnails/MediaInfoControl.java
@@ -0,0 +1,39 @@
+package com.zemiak.nasphotos.thumbnails;
+
+import com.zemiak.nasphotos.commandline.CommandLine;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.inject.Inject;
+
+public class MediaInfoControl {
+    private static final Logger LOG = Logger.getLogger(MediaInfoControl.class.getName());
+    private static final String ROTATED = "Rotation";
+
+    @Inject
+    private String mediaInfoPath;
+
+    public List<String> getMediaInfo(Path path) {
+        List<String> params = Arrays.asList(
+            path.toAbsolutePath().toString()
+        );
+
+        List<String> results = Collections.EMPTY_LIST;
+        try {
+            results = CommandLine.execCmd(mediaInfoPath, params);
+        } catch (IOException | InterruptedException | IllegalStateException ex) {
+            LOG.log(Level.SEVERE, "Cannot run mediainfo for " + path.toString(), ex);
+        }
+
+        return results;
+    }
+
+    public boolean isRotated(Path path) {
+        List<String> results = getMediaInfo(path);
+        return results.stream().filter(line -> line.contains(ROTATED)).findAny().isPresent();
+    }
+}
