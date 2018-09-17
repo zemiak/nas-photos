0889fbae88975fa43905cdb24745ff8d1b19087b
Miroslav Vasko <miroslav.vasko@cargo-partner.com>
2018-09-17 16:30:59 +0200

    PHOTO_PATH used as a prefix in tests

--- src/test/java/com/zemiak/nasphotos/files/PictureControlTest.java
+++ src/test/java/com/zemiak/nasphotos/files/PictureControlTest.java
@@ -12,17 +12,17 @@ public class PictureControlTest {
     @Test
     public void isNotImage() throws Exception {
         Arrays.asList(new String[]{
-            "/Volumes/media/Pictures/YYMM Svadba/website/Fotografie_files/P4010496.jpg",
-            "/Volumes/media/Pictures/2016-inbox/01/Miro iPhone/IMG_2302.JPG",
-            "/Volumes/media/Pictures/Maruska - Janka Foto/2012-04/P1030310.JPG",
-            "/Volumes/media/Pictures/_YYMM Svadba/website/Fotografie_files/P4010496.jpg",
-            "/Volumes/media/Pictures/YYMM Svadba/_website/Fotografie_files/P4010496.jpg",
-            "/Volumes/media/Pictures/YYMM Svadba/website/_Fotografie_files/P4010496.jpg",
-            "/Volumes/media/Pictures/YYMM Svadba/website/Fotografie_files/_P4010496.jpg",
-            "/Volumes/media/Pictures/.YYMM Svadba/website/Fotografie_files/P4010496.jpg",
-            "/Volumes/media/Pictures/YYMM Svadba/.website/Fotografie_files/P4010496.jpg",
-            "/Volumes/media/Pictures/YYMM Svadba/website/.Fotografie_files/P4010496.jpg",
-            "/Volumes/media/Pictures/YYMM Svadba/website/Fotografie_files/.P4010496.jpg",
+            PHOTO_PATH + "YYMM Svadba/website/Fotografie_files/P4010496.jpg",
+            PHOTO_PATH + "2016-inbox/01/Miro iPhone/IMG_2302.JPG",
+            PHOTO_PATH + "Maruska - Janka Foto/2012-04/P1030310.JPG",
+            PHOTO_PATH + "_YYMM Svadba/website/Fotografie_files/P4010496.jpg",
+            PHOTO_PATH + "YYMM Svadba/_website/Fotografie_files/P4010496.jpg",
+            PHOTO_PATH + "YYMM Svadba/website/_Fotografie_files/P4010496.jpg",
+            PHOTO_PATH + "YYMM Svadba/website/Fotografie_files/_P4010496.jpg",
+            PHOTO_PATH + ".YYMM Svadba/website/Fotografie_files/P4010496.jpg",
+            PHOTO_PATH + "YYMM Svadba/.website/Fotografie_files/P4010496.jpg",
+            PHOTO_PATH + "YYMM Svadba/website/.Fotografie_files/P4010496.jpg",
+            PHOTO_PATH + "YYMM Svadba/website/Fotografie_files/.P4010496.jpg",
         }).stream().forEach(picFileName -> {
             assertFalse(picFileName, PictureControl.isImage(Paths.get(picFileName), PHOTO_PATH));
         });
@@ -31,9 +31,9 @@ public class PictureControlTest {
 //    @Test
     public void isImage() throws Exception {
         Arrays.asList(new String[]{
-            "/Volumes/media/Pictures/2014/1201 Deti/photo.Png",
-            "/Volumes/media/Pictures/2089/1202 Vianoce/IMG_2302.JPG",
-            "/Volumes/media/Pictures/2012/1203 Maruska/IMG_0126.JPG"
+            PHOTO_PATH + "2014/1201 Deti/photo.Png",
+            PHOTO_PATH + "2089/1202 Vianoce/IMG_2302.JPG",
+            PHOTO_PATH + "2012/1203 Maruska/IMG_0126.JPG"
         }).stream().forEach(picFileName -> {
             assertTrue(picFileName, PictureControl.isImage(Paths.get(picFileName), PHOTO_PATH));
         });
@@ -42,11 +42,11 @@ public class PictureControlTest {
     @Test
     public void isNotHidden() throws Exception {
         Arrays.asList(new String[]{
-            "/Volumes/media/Pictures/YYMM Svadba/website/Fotografie_files/P4010496.jpg",
-            "/Volumes/media/Pictures/2016-inbox/01/Miro iPhone/IMG_2302.JPG",
-            "/Volumes/media/Pictures/Maruska - Janka Foto/2012-04/P1030310.JPG",
-            "/Volumes/media/Pictures/2014/01 Deti/photo.Png",
-            "/Volumes/media/Pictures/2089/12 Vianoce/IMG_2302.JPG"
+            PHOTO_PATH + "YYMM Svadba/website/Fotografie_files/P4010496.jpg",
+            PHOTO_PATH + "2016-inbox/01/Miro iPhone/IMG_2302.JPG",
+            PHOTO_PATH + "Maruska - Janka Foto/2012-04/P1030310.JPG",
+            PHOTO_PATH + "2014/01 Deti/photo.Png",
+            PHOTO_PATH + "2089/12 Vianoce/IMG_2302.JPG"
         }).stream().forEach(picFileName -> {
             assertFalse(picFileName, PictureControl.isHidden(Paths.get(picFileName)));
         });
@@ -55,14 +55,14 @@ public class PictureControlTest {
     @Test
     public void isHidden() throws Exception {
         Arrays.asList(new String[]{
-            "/Volumes/media/Pictures/_YYMM Svadba/website/Fotografie_files/P4010496.jpg",
-            "/Volumes/media/Pictures/YYMM Svadba/_website/Fotografie_files/P4010496.jpg",
-            "/Volumes/media/Pictures/YYMM Svadba/website/_Fotografie_files/P4010496.jpg",
-            "/Volumes/media/Pictures/YYMM Svadba/website/Fotografie_files/_P4010496.jpg",
-            "/Volumes/media/Pictures/.YYMM Svadba/website/Fotografie_files/P4010496.jpg",
-            "/Volumes/media/Pictures/YYMM Svadba/.website/Fotografie_files/P4010496.jpg",
-            "/Volumes/media/Pictures/YYMM Svadba/website/.Fotografie_files/P4010496.jpg",
-            "/Volumes/media/Pictures/YYMM Svadba/website/Fotografie_files/.P4010496.jpg"
+            PHOTO_PATH + "_YYMM Svadba/website/Fotografie_files/P4010496.jpg",
+            PHOTO_PATH + "YYMM Svadba/_website/Fotografie_files/P4010496.jpg",
+            PHOTO_PATH + "YYMM Svadba/website/_Fotografie_files/P4010496.jpg",
+            PHOTO_PATH + "YYMM Svadba/website/Fotografie_files/_P4010496.jpg",
+            PHOTO_PATH + ".YYMM Svadba/website/Fotografie_files/P4010496.jpg",
+            PHOTO_PATH + "YYMM Svadba/.website/Fotografie_files/P4010496.jpg",
+            PHOTO_PATH + "YYMM Svadba/website/.Fotografie_files/P4010496.jpg",
+            PHOTO_PATH + "YYMM Svadba/website/Fotografie_files/.P4010496.jpg"
         }).stream().forEach(picFileName -> {
             assertTrue(picFileName, PictureControl.isHidden(Paths.get(picFileName)));
         });
