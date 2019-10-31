e6d08021de5d392ab96bcecda2f8255edd5cecb6
zemiak <zemiak@gmail.com>
2019-10-31 18:57:16 +0100

    glightbox selected

--- src/main/resources/META-INF/resources/js/LightBox.js
+++ src/main/resources/META-INF/resources/js/LightBox.js
@@ -1,7 +1,6 @@
 import { TemplateService } from "./TemplateService.js";
 
 /**
- * lightbox/simpleLightbox.js: https://dbrekalo.github.io/simpleLightbox/
  * glightbox/glightbox.js: https://github.com/biati-digital/glightbox
  */
 
@@ -12,16 +11,8 @@ export class LightBox extends HTMLElement {
     }
 
     onRenderLightboxEvent(e) {
-        //setTimeout(this.simpleLightbox, 500);
         setTimeout(this.glightbox, 500);
         console.log("lightbox postponed");
-    }
-
-    simpleLightbox() {
-        console.log("lightboxing");
-        const elements = document.querySelectorAll("li>a");
-        this.lightbox = new SimpleLightbox({elements: elements});
-        console.log("lightboxed", elements);
     }
 
     glightbox() {
