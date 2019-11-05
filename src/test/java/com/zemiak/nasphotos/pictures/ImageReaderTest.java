e4909748a961b84da4729eeab9d276dfed68f4df
Miroslav Vaško <vasko@Miroslavs-Mac-mini.local>
2019-11-05 10:16:47 +0100

    Removed external dependency on fonder.png

--- src/test/java/com/zemiak/nasphotos/pictures/ImageReaderTest.java
+++ src/test/java/com/zemiak/nasphotos/pictures/ImageReaderTest.java
@@ -1,14 +1,11 @@
 package com.zemiak.nasphotos.pictures;
 
 import java.awt.Dimension;
-import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Iterator;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import javax.imageio.ImageIO;
-import javax.imageio.stream.ImageInputStream;
 
 import com.zemiak.nasphotos.files.entity.PictureData;
 
@@ -19,8 +16,6 @@ import io.quarkus.test.junit.QuarkusTest;
 
 @QuarkusTest
 public class ImageReaderTest {
-    private static final Logger LOG = Logger.getLogger(ImageReaderTest.class.getName());
-
     @Test
     public void ratioCalc() throws IOException {
         String photoPath = ConfigProvider.getConfig().getValue("photoPath", String.class);
@@ -28,15 +23,15 @@ public class ImageReaderTest {
             photoPath = "/Volumes/media/Pictures";
         }
 
-        File file = new File(photoPath + "/special/folder.png");
-        if (! file.canRead()) {
+        InputStream folderImageStream = ClassLoader.getSystemResourceAsStream("META-INF/resources/img/folder.png");
+        if (null == folderImageStream) {
             throw new IOException("File folder.png does not exist");
         }
 
         long width, height;
         PictureData data = new PictureData();
 
-        Dimension dimension = getDimensions(file);
+        Dimension dimension = getDimensions(folderImageStream);
         if (null == dimension) {
             return;
         }
@@ -62,24 +57,17 @@ public class ImageReaderTest {
         System.err.println("Data: " + data.toString());
     }
 
-    private Dimension getDimensions(File file) throws IOException {
-        try (ImageInputStream in = ImageIO.createImageInputStream(file)){
-            if (null == in) {
-                LOG.log(Level.SEVERE, "Cannot get a reader for file {0}", file.toString());
-                return null;
+    private Dimension getDimensions(InputStream in) throws IOException {
+        final Iterator<javax.imageio.ImageReader> readers = ImageIO.getImageReaders(in);
+        if (readers.hasNext()) {
+            javax.imageio.ImageReader reader = readers.next();
+            try {
+                reader.setInput(in);
+                return new Dimension(reader.getWidth(0), reader.getHeight(0));
+            } finally {
+                reader.dispose();
             }
 
-            final Iterator<javax.imageio.ImageReader> readers = ImageIO.getImageReaders(in);
-            if (readers.hasNext()) {
-                javax.imageio.ImageReader reader = readers.next();
-                try {
-                    reader.setInput(in);
-                    return new Dimension(reader.getWidth(0), reader.getHeight(0));
-                } finally {
-                    reader.dispose();
-                }
-
-            }
         }
 
         return null;
